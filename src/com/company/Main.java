5a4868dfad017ce8cd4c4e5a78060eea457d4d9e
rezaan6 <rezaan6@gmail.com>
2021-11-28 11:10:39 +0530

    initial commit student marks calculation

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int mk1 = 45;
+    int mk2 = 45;
+    int mk3 = 10;
+    int total = mk1+mk2+mk3;
+        System.out.println("Mark 1 :"+mk1);
+        System.out.println("Mark 2 :"+mk2);
+        System.out.println("Mark 3 :"+mk3);
+        System.out.println("Totol:"+total);
+    }
+}
