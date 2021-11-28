778bfd33cd5bb6b64a6f1c83cf390d35856d784f
rezaan6 <rezaan6@gmail.com>
2021-11-28 11:29:31 +0530

    Average Student marks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,9 +7,11 @@ public class Main {
     int mk2 = 45;
     int mk3 = 10;
     int total = mk1+mk2+mk3;
+    double avg = total / 3;
         System.out.println("Mark 1 :"+mk1);
         System.out.println("Mark 2 :"+mk2);
         System.out.println("Mark 3 :"+mk3);
         System.out.println("Totol:"+total);
+        System.out.println("Average :"+avg);
     }
 }
