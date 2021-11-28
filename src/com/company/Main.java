84192ea0bd758dd23e7e0d6cc2cd421b0e8d8449
rezaan6 <rezaan6@gmail.com>
2021-11-28 11:36:20 +0530

    avg data change Student no add

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        String Stuno="Student No 001";
 	int mk1 = 45;
     int mk2 = 45;
     int mk3 = 10;
     int total = mk1+mk2+mk3;
-    double avg = total / 3;
+    int avg = total / 3;
+    String grd;
+    if (avg>50)
+        grd="Pass";
+    else
+        grd ="Fail";
+        System.out.println("Student No :"+Stuno);
         System.out.println("Mark 1 :"+mk1);
         System.out.println("Mark 2 :"+mk2);
         System.out.println("Mark 3 :"+mk3);
-        System.out.println("Totol:"+total);
+        System.out.println("Total :"+total);
         System.out.println("Average :"+avg);
+        System.out.println("Grade :"+grd);
     }
 }
